b737bd8a860b2e4e3441e5e4fd228cea62641df2
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-10-22 16:00:26 -0700

    Completed tasks

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range (1,101):
-        if num % 3 == 0:
-            print("Fizz", end=" ")
-        elif num % 5 == 0:
-            print("Buzz", end=" ")
-        elif num % 15 == 0:
+    for number in range(1, 101):
+        if number % 15 == 0:
             print("FizzBuzz", end=" ")
+        elif number % 3 == 0:
+            print("Fizz", end=" ")
+        elif number % 5 == 0:
+            print("Buzz", end=" ")
         else:
-            print("{}".format(num), end=" ")
+            print("{:d}".format(number), end=" ")
