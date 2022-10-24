3a4188e4cbac20d64e1dc0233342715646130fc4
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-10-24 15:51:31 -0700

    First tasks

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   from sys import argv
-    args = len(argv) - 1
+    import sys
+    args = len(sys.argv) - 1
     if args == 0:
         print("0 arguments.")
     elif args == 1:
         print("1 argument:")
     else:
-        print("{:d} arguments:".format(args))
+        print("{} arguments:".format(args))
     for count in range(args):
-        print("{:d}: {:d}".format(count + 1, argv[count + 1]))
+        print("{}: {}".format(count + 1, sys.argv[args + 1]))
