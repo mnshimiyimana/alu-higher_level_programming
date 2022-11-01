8da73c14bd09919eb72e4eda24cae78ba6b2c6fb
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-11-01 13:52:35 -0700

    removing letters

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    print(my_string.translate({ord('c'): None}))
-    print(my_string.translate({ord('C'): None}))
+    string = my_string.translate(ord('c'): None})
+    string = my_string.translate(ord('C'): None})
+    string = my_string[:]
+    return my_string
