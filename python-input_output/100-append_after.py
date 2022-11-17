a103513c10f24462f61c9bfe2664d7726120b12c
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-11-17 12:43:37 -0800

    Last commit

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    define a 'append_after'
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+        insert line of text to file after each line containing a string
+    """
+
+    with open(filename, 'r+', encoding='utf-8') as f:
+        lines = []
+        for line in f:
+            lines.append(line)
+            if search_string in line:
+                lines.append(new_string)
+        f.seek(0)
+        for line in lines:
+            f.write(line)
