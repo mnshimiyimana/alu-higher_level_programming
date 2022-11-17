32b37e42909534144d9e683144f284e648b75d40
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-11-17 06:41:08 -0800

    Sorted list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class will inherit from a built-in class
+"""
+
+
+Class Mylist(list):
+    """Prints a sorted list"""
+
+    def print_sorted(self):
+        print(sorted(self))
