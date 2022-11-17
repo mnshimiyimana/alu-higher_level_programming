940e3493e92c83167a59105fe3099e5a9a72ffac
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-11-17 06:31:09 -0800

    returns a list object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-    defining a function 'lookup'
+    define a function 'lookup'
 """
 def lookup(obj):
     """
-       Return list object
+        Returns list object
     """
-    return(dir(obj))
+    return (dir(obj))
