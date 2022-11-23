884fd386fc2b9ab66a402a9e8a8d3481eecc8add
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-11-23 17:12:53 +0200

    Response header value

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Create a Python script that sends a request to the URL."""
+
+
+import sys
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
+
+
