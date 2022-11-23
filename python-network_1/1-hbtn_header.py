bd66180ea8df40cbcaea3999f58bbc2e54397c71
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-11-23 17:20:04 +0200

    Response header value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -8,6 +8,5 @@ import urllib.request
 
 if __name__ == '__main__':
     with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.headers.get('X-Request-Id'))
-
-
+        headers = response.info()
+        print(headers['X-Request-Id'])
