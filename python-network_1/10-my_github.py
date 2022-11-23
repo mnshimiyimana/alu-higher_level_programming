1960c12ae2d62aaf9864dca790e10040dfce52c0
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-11-23 17:11:38 +0000

    Network 1

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""takes my Github credentials (username and password) and uses the Github API to display your id"""
+
+
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = requests.get(url, auth=HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
+    try:
+        print(r.json().get('id'))
+    except:
+        print("None")
---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request """
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    try:
+        letter = sys.argv[1]
+    except IndexError:
+        letter = ""
+        response = requests.post("http://0.0.0.0:5000/search_user",
+        data={"q": lett}
+        )
+    try:
+            json_response = response.json()
+        if response.headers.get("Content-Type") == 'application/json':
+            if len(json_response) > 0:
+                print("[{}] {}".format(
+                    json_response["id"],
+                    json_response["name"])
+                )
+            else:
+                print("No result")
+    except:
+        print("Not a valid JSON")
