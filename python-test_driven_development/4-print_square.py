db14e5763ef3b93357e36488bf9adc29f616b435
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-12-06 15:34:26 -0800

    First tasks

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,18 +5,15 @@
 
 def print_square(size):
 
-    """Function that prints a square with the character #"""
+    """Function that prints a square with the character"""
 
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         # if size is negative
         raise ValueError("size must be >= 0")
-    if size == 0:
-        # if size is 0
-        print()
     for i in range(size):
         # print the square
         for j in range(size):
-            print("#", end="")
+            print("#" * size)
         print()
