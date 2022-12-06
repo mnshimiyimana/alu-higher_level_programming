585833d922af0e7a25568e1d07f60ab8bd9564f9
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-12-06 02:51:37 -0800

    First tasks

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,12 +7,12 @@ def add_integer(a, b=98):
     """Add two integers and return the result."""
 
     if type(a) is not int and type(a) is not float:
-       raise TypeError("a must be an integer")
+      raise TypeError("a must be an integer")
     if type(b) is not int and  type(b) is not float:
-       raise TypeError("b must be an integer")
+      raise TypeError("b must be an integer")
     for i in (a, b):
         if type(a) is float:
-            a = int(a)
+           a = int(a)
         if type(b) is float:
-            b = int(b)
+           b = int(b)
     return a + b
