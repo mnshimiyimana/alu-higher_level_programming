e8d2beb931cd34436dfb04c4420ad04ee0366ce9
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-12-06 13:56:18 -0800

    First tasks

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,13 +3,12 @@
 '''Function that prints my name'''
 
 
-if not isinstance(first_name, str):
-
 def say_my_name(first_name, last_name=""):
     '''Function that prints my name'''
-    if not isinstance(first_name, str): # TypeError: isinstance() arg 2 must be a type or tuple of types
+    if type(first_name) is not str: 
+        # TypeError: isinstance() arg 2 must be a type or tuple of types
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
          # TypeError: isinstance() arg 2 must be a type or tuple of types
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
