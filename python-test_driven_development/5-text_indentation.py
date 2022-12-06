7545cc640c685f7362fddcd148c64cefd54fc042
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-12-06 12:50:05 -0800

    First tasks

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-"""Function that prints a text with 2 new lines after each of these characters"""
+"""Function that prints a text with 2 new lines after each of these character"""
 
 
 def text_indentation(text):
-    """Function that prints a text with 2 new lines"""
-
+    """Function that prints a text with 2 new lines after each of these characters: ., ? and :"""
     if type(text) is not str: # if text is not a string
         raise TypeError("text must be a string")
-    for i in range(len(text)):  # for each character in text
-        if text[i] == "." or text[i] == "?" or text[i] == ":":
-            print(text[i])
+        c = 0
+        while c < len(text):
+          if text[c] == "." or text[c] == "?" or text[c] == ":":
+             print(text[c])
+                print()
+                c += 1
+             else:
+                print(text[c], end="")
+                c += 1
+        if text[c] == "." or text[c] == "?" or text[c] == ":":
+            print(text[c])
             print()
-        else: # if the character is not ., ? or :
-            print(text[i], end="")
+        else:
+            print(text[c], end="")
+        c += 1
+    print()
+
