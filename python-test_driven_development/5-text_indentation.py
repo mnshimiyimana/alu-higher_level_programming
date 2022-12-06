7ad56d49f5d64edd85d2ed5a89df79b8adc44671
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-12-06 13:13:48 -0800

    First tasks

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,7 @@
 
 
 def text_indentation(text):
-    """Function that prints a text with 2 new lines """    
+    """Function that prints a text with 2 new lines """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
