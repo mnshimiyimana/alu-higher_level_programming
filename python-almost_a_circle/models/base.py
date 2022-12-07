1de4fe3828d86d230defb520e918481aa58ab58a
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-12-07 06:30:39 -0800

    First tasks

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,11 +7,57 @@ class Base:
 nb_objects = 0
 
     def __init__(self, id=None):
-        # Base class constructor
+        """ Constructor documentation"""
         self.id = id
         if id is not None:
             self.id = id
         else:
-            # increment the number of objects
+            """Increment the number of objects"""
             Base.nb_objects += 1
             self.id = Base.nb_objects
+
+     @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        with open
+
+            (filename, "w") as f:
+            if list_objs is None:
+                f.write(cls.to_json_string([]))
+            else:
+                f.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string"""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string) 
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as f:
+                return [cls.create(**obj) for obj in cls.from_json_string(f.read())]
+        except:
+            return []
