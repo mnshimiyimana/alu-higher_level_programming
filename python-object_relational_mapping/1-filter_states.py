fd371fafd53f3f23b08f98c461c374023435b171
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-12-09 10:06:27 -0800

    Hectic

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
+"""Lists all states with starting name with N from the databse"""
+   
 
 import MySQLdb
-from sys import argv
+import sys
 
-'''
-lists all states with starting name with N
-from the database hbtn_0e_0_usa
-'''
+
 if __name__ == "__main__":
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
