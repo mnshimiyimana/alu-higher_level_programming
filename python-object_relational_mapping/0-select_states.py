fedc7f39b7e4a6b6749f9facbc063e7ab134cd2f
mnshimiyimana <m.nshimiyim@alustudent.com>
2022-12-09 09:31:29 -0800

    Hectic

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,6 +7,7 @@ from sys import argv
 a script that lists all states
 from the database
 '''
+
 if __name__ == "__main__":
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
