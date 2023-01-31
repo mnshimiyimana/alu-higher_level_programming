f8899fc2cda1c551da043c0d2342e8e6b074ba28
Maureen <113348335+mnshimiyimana@users.noreply.github.com>
2023-01-31 13:24:22 +0000

    Arguments

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-const numArgv = process.argv(3);
-if (numArgv == null) {console.log('No argument'); }
- else { console.log(numArgv); }
- 
+const numArgv = process.argv(2);
+if (numArgv == null) {console.log('No argument'); } else { console.log(numArgv); }
