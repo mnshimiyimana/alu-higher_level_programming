937f70c80a84e5bf513530592986d2186fc544a5
Maureen <113348335+mnshimiyimana@users.noreply.github.com>
2023-01-31 20:45:12 +0000

    Arguments + Sentences

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const firstArgv = process.argv[2];
-const secondArgv = process.argv[3];
-console.log(firstArgv + ' is ' + secondArgv);
+const word1 = process.argv[2];
+const word2 = process.argv[3];
+console.log(`${word1} is ${word2}`);
