ae2614bfc50c563d311a26be2892356eb1b76183
mnshimiyimana <m.nshimiyimana@alustudent.com>
2023-02-01 12:58:34 -0800

    An integer

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-const firstArgv = process.argv[2];
-const secondArgv = process.argv[3];
-console.log(firstArgv + ' is ' + secondArgv);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
-if (Number.isNaN(num)) { console.log('Not a number'); } else { console.log('My number: ' + num); }
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
