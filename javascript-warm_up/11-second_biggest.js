b1edf1e466b15be548895910ff58e349c8ba4bf2
mnshimiyimana <m.nshimiyimana@alustudent.com>
2023-02-03 07:09:03 -0800

    Second biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,7 @@
 
 const args = process.argv.slice(2);
 if (args.length <= 1) {
-	console.log(0);
-}
-else {
-console.log(args.sort((a, b) => b - a)[1]);
+  console.log(0);
+} else {
+  console.log(args.sort((a, b) => b - a)[1]);
 }
