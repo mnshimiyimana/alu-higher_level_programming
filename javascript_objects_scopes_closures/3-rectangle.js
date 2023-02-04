026692928103c69092b7d7a663cdc6af43fb4601
mnshimiyimana <m.nshimiyimana@alustudent.com>
2023-02-04 00:45:03 -0800

    Executable

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
-
 class Rectangle {
-  constructor (h, w) {
-    if (h > 0 && w > 0) {
-      this.height = h;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
+      this.height = h;
     }
   }
-}
-
 
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+}
 module.exports = Rectangle;
