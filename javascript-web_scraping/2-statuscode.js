64a7cd8857bf66a80ac0731a16cd4acd7bd62efa
mnshimiyimana <m.nshimiyim@alustudent.com>
2023-02-07 10:08:03 +0000

    Web Scarping

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const args = process.argv;
-let request = require('request');
-request(args[2], function (error, response, body) {
-  if (error) {
-    console.log('error:', error;
-  } else console.log('code:', response && response.statusCode);
+const request = require('request');
+request.get(process.argv[2], (err, response) => {
+  if (err) console.log(err);
+  console.log(`code: ${response.statusCode}`);
 });
