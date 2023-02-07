2745f2695237b4a35fd467c8c4278597c8074f59
m.nshimiyimana <m.nshimiyim@alustudent.com>
2023-02-07 09:47:30 +0000

    README.md

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const file = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(file, 'utf8', (error, data) => {
-if (error) {
+  if (error) {
     console.log(error);
   } else {
     console.log(data);
